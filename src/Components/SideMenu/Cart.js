710872901ee4258cc02879ec5f8dbf416a361536
JPruezkiez1 <jpruezkiez@icloud.com>
2023-10-10 23:17:35 -0500

    fix 0.2

--- src/Components/SideMenu/Cart.js
+++ src/Components/SideMenu/Cart.js
@@ -51,7 +51,7 @@ const CheckoutSideMenu = () => {
                     quantity: product.quantity
                 }))
             };
-            const response = await fetch('https://jpruezkiez.azurewebsites.net/add-order', {
+            const response = await fetch('http://34.75.37.247:8080/add-order', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
